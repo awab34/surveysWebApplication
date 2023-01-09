579a0035e281b1d4b6c8aac48f79c63a20cb71e1
awab34 <awab3468@gmail.com>
2023-01-09 00:53:06 -0800

    surveys completed with questions insertion

--- laravel_surveys_api/app/Models/Survey.php
+++ laravel_surveys_api/app/Models/Survey.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Survey extends Model
 {
-    use HasFactory;
+    use HasFactory,HasSlug;
+    const TYPE_TEXT = 'text';
+    const TYPE_TEXTAREA = 'textarea';
+    const TYPE_SELECT = 'select';
+    const TYPE_RADIO = 'radio';
+    const TYPE_CHECKBOX = 'checkbox';
+    protected $fillable = ['title','image','slug','status','description','expire_date','user_id'];
+    public function getSlugOptions(): SlugOptions{
+        return SlugOptions::create()->generateSlugsForm('title')->saveSlugsTo('slug');
+    }
+    public function questions(){
+        return $this->hasMany(SurveyQuestion::class);
+        
+    }
 }
